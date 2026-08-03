3c96e5b841df1e4d67a2f515ebe94c9f10ed9e09
Giorubio27 <Giorubio27@gmail.com>
2026-08-03 11:43:01 -0600

    commited 2 tests

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -6,23 +6,64 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 public class PriorityQueueTests
 {
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: I want to test to see if the add works in my priority queue tests
+    // Expected Result: "5", 5
+    // Defect(s) Found: 1. they skip the last item in the priority queue, they arent picking up the highpriority index
+    // 2. 
+
     public void TestPriorityQueue_1()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        priorityQueue.Enqueue("5", 5);
+        priorityQueue.Enqueue("2", 1);
+        priorityQueue.Enqueue("8", 8);
+
+        var result = priorityQueue.Dequeue();
+
+        Assert.AreEqual("8", result);
+
     }
 
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
+    // Scenario: Having 2 max priorities and having the FIFO rule apply still
+    // Expected Result: 8, 8 
     // Defect(s) Found: 
     public void TestPriorityQueue_2()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        priorityQueue.Enqueue("5", 5);
+        priorityQueue.Enqueue("2", 1);
+        priorityQueue.Enqueue("8a", 8);
+        priorityQueue.Enqueue("5", 5);
+        priorityQueue.Enqueue("8b", 8);
+
+        var firstRemoved = priorityQueue.Dequeue();
+
+        Assert.AreEqual("8a", firstRemoved, "The first number removed was the first 8");
+
+
+    }
+
+    [TestMethod]
+    // Scenario: I want to test to see if the dequeue works when the queue is empty
+    // Expected Result: Noone in queue response
+    // Defect(s) Found: 
+    public void TestPriorityQueue_3()
+    {
+        var priorityQueue = new PriorityQueue();
+
+        try
+        {
+            priorityQueue.Dequeue();
+            Assert.Fail("Exception should have been thrown");
+
+        }
+        catch (InvalidOperationException e)
+        {
+            Assert.AreEqual("The queue is empty.", e.Message);
+
+        }
+
     }
 
     // Add more test cases as needed below.
