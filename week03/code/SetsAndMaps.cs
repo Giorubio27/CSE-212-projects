bfcc1d4a68a6b3b828057fa4be7112add3bcae61
Giorubio27 <Giorubio27@gmail.com>
2026-08-05 07:55:04 -0600

    starting code for degrees

--- week03/code/SetsAndMaps.cs
+++ week03/code/SetsAndMaps.cs
@@ -68,6 +68,7 @@ public static class SetsAndMaps
         foreach (var line in File.ReadLines(filename))
         {
             var fields = line.Split(",");
+            
             // TODO Problem 2 - ADD YOUR CODE HERE
         }
 
