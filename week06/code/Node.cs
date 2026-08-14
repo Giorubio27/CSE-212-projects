31310910cc4eb23d15bdf537171d4c9d13b7999f
Giorubio27 <Giorubio27@gmail.com>
2026-08-13 18:18:27 -0600

    i have accomplished everything up to tree height

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -21,7 +21,7 @@ public class Node
             else
                 Left.Insert(value);
         }
-        else
+        else if (value > Data)
         {
             // Insert to the right
             if (Right is null)
@@ -29,17 +29,57 @@ public class Node
             else
                 Right.Insert(value);
         }
+
     }
 
     public bool Contains(int value)
     {
         // TODO Start Problem 2
+        if (value == Data)
+        {
+            return true;
+        }
+        else if (value < Data)
+        {
+            if (Left is null)
+            {
+                return false;
+            }
+            else
+            {
+                return Left.Contains(value);
+            }
+        }
+        else if (value > Data)
+        {
+            if (Right is null)
+            {
+                return false;
+            }
+            else
+            {
+                return Right.Contains(value);
+            }
+        }
         return false;
     }
 
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = 0;
+        if (Left is not null)
+        {
+            leftHeight = Left.GetHeight();
+        }
+
+        int rightHeight = 0;
+        if (Right is not null)
+        {
+            rightHeight = Right.GetHeight();
+        }
+
+        return 1 + Math.Max(leftHeight, rightHeight);
+        // Replace this line with the correct return statement(s)
     }
 }
